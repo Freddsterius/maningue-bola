f1846ec462bb69d235886d0f8c3e4a141035ebf7
freddsterius <freddsteriusfreddster@gmail.com>
2025-07-18 16:22:03 +0200

    added some placeholder text on the Sobre page

--- src/pages/sobre/Sobre.js
+++ src/pages/sobre/Sobre.js
@@ -20,6 +20,24 @@ const Sobre = () => {
           <i>LaLiga</i>.
         </a>
       </p>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo velit
+        nihil, obcaecati neque adipisci voluptas mollitia, nemo facilis quo
+        numquam eaque totam id vel reprehenderit molestias laboriosam pariatur
+        ex alias similique voluptate nulla amet? Libero, nobis architecto nihil
+        eveniet quod obcaecati necessitatibus itaque odio illo aliquid deleniti
+        incidunt, reiciendis, beatae quo magni magnam animi voluptatum illum
+        accusamus! Pariatur provident perferendis adipisci magni eius dolorem
+        cumque placeat suscipit et ab perspiciatis dolore assumenda, nisi ullam
+        libero quod excepturi deleniti temporibus incidunt molestias similique.
+        Quam, beatae tenetur. Ea sit eligendi optio qui sint nisi similique
+        consequatur nulla delectus velit officia at aut blanditiis autem
+        quibusdam ipsam cupiditate, iure quis cum asperiores voluptatem odit
+        nesciunt adipisci modi? Reprehenderit voluptatibus perferendis alias
+        dolor vitae ducimus, aliquam fugiat quos hic illum! Pariatur vel, dolore
+        illo ab id modi voluptatem, dolor enim fugiat beatae similique aliquid
+        ea quos eveniet aspernatur quisquam quaerat rem. Autem, eum ipsam?
+      </div>
     </div>
   );
 };
